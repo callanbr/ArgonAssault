b4c7e21235b1fce3683e6de9620f29470188f286
callanbr <chrisallan1992@icloud.com>
2020-01-01 03:26:05 -0700

    OffsetThisFrame

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour{
+
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 4f;
+
     // Start is called before the first frame update
     void Start(){
         
@@ -11,7 +14,8 @@ public class Player : MonoBehaviour{
 
     // Update is called once per frame
     void Update(){
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        print(horizontalThrow);
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffsetThisFrame = xThrow * xSpeed * Time.deltaTime;
+        print(xOffsetThisFrame);
     }
 }
