d942a336cb6dc995fba207e2606506097a0ecb80
callanbr <chrisallan1992@icloud.com>
2020-01-01 16:34:37 -0700

    limit x movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,6 +6,7 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour{
 
     [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 4f;
+    [Tooltip("In m")] [SerializeField] float xRange = 7f;
 
     // Start is called before the first frame update
     void Start(){
@@ -16,8 +17,9 @@ public class Player : MonoBehaviour{
     void Update(){
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         float xOffset = xThrow * xSpeed * Time.deltaTime;
-        float rawNewXPos = transform.localPosition.x + xOffset;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange - .5f, xRange);
 
-        transform.localPosition = new Vector3(rawNewXPos, transform.localPosition.y, transform.localPosition.z);
+        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
     }
 }
