c653a8cbcb6a42b614fd8ddbf008cc934a05eedf
callanbr <chrisallan1992@icloud.com>
2020-01-02 11:41:48 -0700

    changed spped and range x

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -18,7 +18,7 @@ public class Player : MonoBehaviour{
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         float xOffset = xThrow * xSpeed * Time.deltaTime;
         float rawXPos = transform.localPosition.x + xOffset;
-        float clampedXPos = Mathf.Clamp(rawXPos, -xRange - .5f, xRange);
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
 
         transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
     }
