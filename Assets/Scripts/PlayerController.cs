490bb9fa5e3610b4ea9ef1edddb87915ba5654f1
callanbr <chrisallan1992@icloud.com>
2020-01-13 17:40:12 -0700

    added ship particles

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -62,22 +62,23 @@ public class PlayerController : MonoBehaviour{
 
     private void ProcessFiring() {
         if (CrossPlatformInputManager.GetButton("Fire")) {
-            ActivateGuns();
+            SetGunsActive(true);
         } else {
-            DeactivateGuns();
+            SetGunsActive(false);
         }
     }
 
-    private void ActivateGuns() {
+    private void SetGunsActive(bool isActive) {
         foreach(GameObject gun in guns) {
-            gun.SetActive(true);
+            var emissionModule = gun.GetComponent<ParticleSystem>().emission;
+            emissionModule.enabled = isActive;
         }
     }
 
-    private void DeactivateGuns() {
-        foreach (GameObject gun in guns) {
-            gun.SetActive(false);
-        }
-    }
+    // private void DeactivateGuns() {
+    //     foreach (GameObject gun in guns) {
+    //         gun.SetActive(false);
+    //     }
+    // }
 
 }
